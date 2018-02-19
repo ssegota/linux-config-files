514099521cc129c2100e65de78a17ab32d2de3eb
Sandi <ssegota@gmail.com>
2018-02-19 14:42:38 +0100

    Initial Commit

---
+++ .vimrc
@@ -0,0 +1,25 @@
+"Turn on line numbers
+set number
+"Turn on filetype based indent
+filetype indent on
+"Don't redraw during macros
+set lazyredraw
+"Highlight matching brackets
+set showmatch
+"set cursorline
+set cursorline
+"enable syntax processing
+syntax enable
+"set backup directory as /tmp
+set backup
+set backupdir=~/.vim-tmp,~/.tmp,~/tmp,/var/tmp,/tmp
+set backupskip=/tmp/*,/private/tmp/*
+set directory=~/.vim-tmp,~/.tmp,~/tmp,/var/tmp,/tmp
+set writebackup
+set background=dark
+set t_Co=256
+"Change colorscheme
+"colorscheme badwolf
+colorscheme molokai
+"colorscheme goodwolf 
+
---
+++ setup.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#from official repos
+apt install i3
+apt install git
+apt install vim
+apt install gparted
+apt install htop
+apt install terminator
+apt install cmus
+apt install ubuntu-restricted-extras
+
+#--------------------
+#i3 gaps
+#--------------------
+
+#dependancies
+apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool
+
+add-apt-repository ppa:aguignard/ppa
+apt-get update
+apt-get install libxcb-xrm-dev
+
+#clone the repo and install
+
+mkdir ~/temp
+cd temp
+
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+make install
+
+#--------------------
+#config files
+#--------------------
+#might need to copy them manually
+
+cd .. #go back to temp
+git clone https://www.github.com/ssegota/linux-config-files 
+cd linux-config-files
+#copy config files to home
+cp .vim .vimrc .bashrc ~/
+#copy to .config directory
+cp .config/i3/* ~/.config/i3/
+cp .config/terminator/* ~/.config/terminator/
